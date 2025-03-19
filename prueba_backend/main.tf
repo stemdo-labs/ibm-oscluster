8f92bc5a8d028ce4f7dce8881865d89f5b221197
rafagll <rafaelgonzalezllamas2001@gmail.com>
2025-03-19 11:17:17 +0100

    terraform de pruebas

---
+++ prueba_backend/main.tf
@@ -0,0 +1,45 @@
+##############################################################################
+# Terraform Providers
+##############################################################################
+
+terraform {
+  backend "s3" {
+    bucket = "gobernanza-ibm-bkt"
+    key    = "cluster/terraform.tfstate"
+    region = "us-east-1"  
+    endpoints = {
+      s3 = "https://s3.eu-es.cloud-object-storage.appdomain.cloud"
+    }
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation = true
+    skip_requesting_account_id  = true
+    use_path_style = true
+    skip_s3_checksum = true
+  }
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+##############################################################################
+
+##############################################################################
+# IBM Cloud Provider
+##############################################################################
+
+provider "ibm" {
+  region = var.region
+  # uncomment if using local terraform
+  ibmcloud_api_key = var.ibm_api_key
+}
+
+##############################################################################
+
+# Name of resource group
+data "ibm_resource_group" "resource_group" {
+  name = var.resource_group
+}
---
+++ prueba_backend/variables.tf
@@ -0,0 +1,5 @@
+variable "resource_group" {
+  type        = string
+  default     = "Stemdo_Sandbox"
+  description = "The resource group to deploy the resources to."
+}
