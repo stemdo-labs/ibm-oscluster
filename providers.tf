66581bc546ce03e650349ea6cf6f123714ccab76
rafagll <rafaelgonzalezllamas2001@gmail.com>
2025-03-19 16:14:56 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -3,20 +3,20 @@
 ##############################################################################
 
 terraform {
-  backend "s3" {
-    bucket = "gobernanza-ibm-bkt"
-    key    = "cluster/terraform.tfstate"
-    region = "us-east-1"  
-    endpoints = {
-      s3 = "https://s3.eu-es.cloud-object-storage.appdomain.cloud"
-    }
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    skip_region_validation = true
-    skip_requesting_account_id  = true
-    use_path_style = true
-    skip_s3_checksum = true
-  }
+  # backend "s3" {
+  #   bucket = "gobernanza-ibm-bkt"
+  #   key    = "cluster/terraform.tfstate"
+  #   region = "us-east-1"  
+  #   endpoints = {
+  #     s3 = "https://s3.eu-es.cloud-object-storage.appdomain.cloud"
+  #   }
+  #   skip_credentials_validation = true
+  #   skip_metadata_api_check     = true
+  #   skip_region_validation = true
+  #   skip_requesting_account_id  = true
+  #   use_path_style = true
+  #   skip_s3_checksum = true
+  # }
   required_providers {
     ibm = {
       source = "IBM-Cloud/ibm"
