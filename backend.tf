d0aefcb551b98a6126115b20f8319d656ccd0a4d
claudiatn <cntenorio@stemdo.io>
2026-06-24 13:50:09 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,14 +3,13 @@ terraform {
     bucket                      = "tfstate-cluster-bootcamp"
     key                         = "bootcamp/cluster/terraform.tfstate"
     region                      = "eu-es"
-    endpoints = {
-      s3 = "https://s3.eu-es.cloud-object-storage.appdomain.cloud"
-    }
+    endpoints                   = "https://s3.eu-es.cloud-object-storage.appdomain.cloud"
+    
 
     # Parámetros para IBM Cloud COS para desactivar validaciones
     skip_credentials_validation = true  # 
     skip_metadata_api_check     = true
     skip_region_validation      = true
-    skip_requesting_account_id  = true 
+   # skip_requesting_account_id  = true 
   }
 }
