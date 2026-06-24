af9d2e1b8c33e8f517afe502aa47b9bcc4bbcab6
claudiatn <cntenorio@stemdo.io>
2026-06-24 15:27:59 +0200

    Comment out ibm_cr_namespace resource in main.tf
    Comment out the ibm_cr_namespace resource block.

--- main.tf
+++ main.tf
@@ -84,7 +84,7 @@ resource "ibm_resource_instance" "cos_instance" {
   resource_group_id               = data.ibm_resource_group.resource_group.id
 }
 
-resource "ibm_cr_namespace" "cr_namespace" {
-    name = "crpruebas"
-    resource_group_id = data.ibm_resource_group.resource_group.id
-}
+#resource "ibm_cr_namespace" "cr_namespace" {
+#    name = "crpruebas"
+#    resource_group_id = data.ibm_resource_group.resource_group.id
+#}
